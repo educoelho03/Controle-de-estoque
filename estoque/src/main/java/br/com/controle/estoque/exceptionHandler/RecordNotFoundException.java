5a294e86a5d6a151fde0b21f5794e322193d39cb
ecoelho03 <ecoelho@contanciainvest.com.br>
2023-11-21 13:29:26 -0300

    improving Exception class

--- estoque/src/main/java/br/com/controle/estoque/exceptionHandler/RecordNotFoundException.java
+++ estoque/src/main/java/br/com/controle/estoque/exceptionHandler/RecordNotFoundException.java
@@ -1,9 +1,6 @@
 package br.com.controle.estoque.exceptionHandler;
 
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 public class RecordNotFoundException extends RuntimeException{
-
     public RecordNotFoundException(String message) {
         super(message);
     }
